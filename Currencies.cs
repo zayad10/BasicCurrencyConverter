04e5384a39b2a66862766e23770df8a03d413735
Zayad <zayadk10@gmail.com>
2024-04-09 23:55:54 +0100

    Add new enums file to represent each currency incl. new cryptos.

---
+++ Currencies.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Currency_Converter
+{
+    public enum Currencies
+    {
+        GBP, //0
+        EUR, //1
+        USD, //2
+        BTC, //3
+        ETH, //4
+        LTC  //5
+
+    }
+}
